2a88239fc54372e3662ff3a6e73b20be3b7a528c
Ilya Brant <detroitib31@gmail.com>
2023-01-23 22:32:19 +0600

    Написал цикл для Физ Баз

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,21 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
+    while (true) {
+        print("Введите Число: ")
+        val reader = Scanner(System.`in`)
+        val number = reader.nextInt()
+        if (number % 3 == 0 && number % 5 == 0) {
+            println("Fizz Buzz")
+        } else if (number % 3 == 0) {
+            println("Fizz")
+        } else if (number % 5 == 0) {
+            println("Buzz")
+        } else {
+            println(number)
+        }
+    }
+}
+
+
 
